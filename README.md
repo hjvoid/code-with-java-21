766c1fff8644022981a5a8362b419477ace04d37
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2024-04-03 16:36:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java 21 Tutorial Project
+
+Welcome to the Java 21 Tutorial Project! This project contains exercises from the book **Code with Java 21** by Aaron Ploetz.
---
+++ src/main/java/chapter2/HelloWorld.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class HelloWorld {
+
+    private static String firstName = "Aaron";
+    private int age = 47;
+
+    public void main(){
+
+        System.out.println("Haelo wurld!");
+        System.out.printf("Welcome to the world of Java, %s!\n", firstName);
+        System.out.printf("age = %d. It's never too late to learn Java!\n", age);
+
+    }
+}
