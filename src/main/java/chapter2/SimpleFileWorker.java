f77bab603afc3ab1583d1a527548e1e13f06ff78
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2024-04-04 14:47:15 +0100

    Add SimpleFileWorker

--- src/main/java/chapter2/SimpleFileWorker.java
+++ src/main/java/chapter2/SimpleFileWorker.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class SimpleFileWorker {
 
@@ -23,6 +25,31 @@ public class SimpleFileWorker {
             writer.write("Adventure, Atari, 1980\n");
             writer.close();
             System.out.println("Writing completed!");
+
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader("gamesCatalog.txt"));
+                String gameLine = reader.readLine();
+                boolean headerRead = false;
+
+                while (gameLine != null) {
+                    if (headerRead) {
+                        String[] gameColumns = gameLine.split(",");
+                        int year = Integer.parseInt(gameColumns[2].trim());
+
+                        if (year == 1981) {
+                            System.out.println(gameLine);
+                        }
+                    } else {
+                        headerRead = true;
+                    }
+
+                    //read the next line
+                    gameLine = reader.readLine();
+                }
+            } catch (IOException readerEx) {
+                System.out.println("Error occurred while writing:");
+                readerEx.printStackTrace();
+            }
 
         } catch (IOException writerEx) {
 
