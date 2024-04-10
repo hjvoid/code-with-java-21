285458fd82dd922e5895ec97b4e30c8a5ee8b168
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2024-04-10 17:08:51 +0100

    Add RPGSimulation with Hero and Player classes

--- src/main/java/chapter4/RPGSimulation.java
+++ src/main/java/chapter4/RPGSimulation.java
@@ -8,7 +8,7 @@ import java.util.TreeMap;
 
 public class RPGSimulation {
 
-    record Monster (String name, int attack, int maxDamage, int defense) {
+    record Monster(String name, int attack, int maxDamage, int defense) {
         static Random random = new Random();
         static int hitPoints = 2;
         static boolean alive = true;
@@ -19,6 +19,14 @@ public class RPGSimulation {
 
         public int rollDamage() {
             return random.nextInt(maxDamage) + 1;
+        }
+
+        public void decrementHitPoints(int damage) {
+            hitPoints = hitPoints - damage;
+
+            if (hitPoints <= 0) {
+                alive = false;
+            }
         }
 
         public boolean isAlive() {
@@ -57,5 +65,93 @@ public class RPGSimulation {
             }
         }
 
+        Hero byorki = new Hero("Byorki", 8, 5, 5);
+        Hero klar = new Hero("Klar", 10, 12, 3);
+        Hero tyrenni = new Hero("Tyrenni", 6, 6, 6, spellbook);
+
+        List<Hero> heroes = new ArrayList<>();
+        heroes.add(byorki);
+        heroes.add(klar);
+        heroes.add(tyrenni);
+
+        List<Object> playerOrder = generatePlayerOrder(heroes, monsters);
+
+        for (Object player : playerOrder) {
+            System.out.println();
+            if (player instanceof Hero hero) {
+                if (hero.isAlive()) {
+                    String name = hero.getName();
+
+                    // pick a random monster
+                    int monsterIndex = randomNumber.nextInt(monsters.size());
+                    Monster targetMonster = monsters.get(monsterIndex);
+
+                    System.out.println(name + " attacks " + targetMonster.name());
+
+                    int attack = hero.rollAttack();
+                    if (attack >= targetMonster.defense) {
+                        int damage = hero.rollDamage();
+                        System.out.println(name + " rolls a " + attack + " and hits " + targetMonster.name() + " for " + damage + " points.");
+                        targetMonster.decrementHitPoints(damage);
+
+                        if (!targetMonster.isAlive()) {
+                            System.out.println(targetMonster.name() + " is down!");
+                        }
+                    } else {
+                        System.out.println(name + " rolls a " + attack + " and misses " + targetMonster.name());
+                    }
+                }
+            } else if (player instanceof Monster monster) {
+                if (monster.isAlive()) {
+                    String name = monster.name;
+                    int heroIndex = randomNumber.nextInt(heroes.size());
+                    Hero targetHero = heroes.get(heroIndex);
+                    String heroName = targetHero.getName();
+
+                    System.out.println(monster.name() + "attacks" + heroName);
+
+                    int attack = monster.rollAttack();
+                    if (attack >= targetHero.getDefense()) {
+                        int damage = monster.rollDamage();
+                        System.out.println(name + " rolls a " + attack + " and hits " +
+                                heroName + " for " + damage + " points.");
+                        targetHero.decrementHitPoints(damage);
+
+                        if (!targetHero.isAlive()) {
+                            System.out.println(heroName + " is down!");
+                        }
+                    } else {
+                        System.out.println(name + " rolls a " + attack + " and misses " + heroName);
+                    }
+                }
+            }
+        }
+    }
+
+    private static List<Object> generatePlayerOrder(List<Hero> heroList, List<Monster> monsterList) {
+        List<Hero> tempHeroList = new ArrayList<Hero>(List.copyOf(heroList));
+        List<Monster> tempMonsterList = new ArrayList<Monster>(List.copyOf(monsterList));
+
+        List<Object> returnValue = new ArrayList<>();
+        Random random = new Random();
+        int playerCount = tempHeroList.size() + monsterList.size();
+
+        while (returnValue.size() < playerCount) {
+            if (random.nextBoolean()) {
+                if (!tempHeroList.isEmpty()) {
+                    int heroIndex = random.nextInt(tempHeroList.size());
+                    returnValue.add(tempHeroList.get(heroIndex));
+                    tempHeroList.remove(heroIndex);
+                } else {
+                    if (!tempMonsterList.isEmpty()) {
+                        int monsterIndex = random.nextInt(tempMonsterList.size());
+
+                        returnValue.add(tempMonsterList.get(monsterIndex));
+                        tempMonsterList.remove(monsterIndex);
+                    }
+                }
+            }
+        }
+        return returnValue;
     }
 }
